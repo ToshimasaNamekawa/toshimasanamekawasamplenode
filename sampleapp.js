e741d06e1346345aaf7b7e2355592975de6c6643
行川 敏正 <toshimasa@namekawa-no-iMac-2.local>
2017-06-20 19:40:19 +0900

    fixed

--- sampleapp.js
+++ sampleapp.js
@@ -1,18 +1,26 @@
-var http = require('http');
+
+re('http');
 var fs = require('fs');
  
 var server = http.createServer();
 server.on('request', doRequest);
-server.listen(1234);
+server.listen(process.env.PORT, process.env.IP);
 console.log('Server running!');
  
 // リクエストの処理
 function doRequest(req, res) {
+    var number = Math.floor(Math.random() * 3);
     fs.readFile('./hello.html', 'UTF-8', 
         function(err, data) {
+            var title = ["ページA", "ページB", "ページC"];
+            var content = ["※これはサンプルで作ったものです。",
+                "もう一つのコンテンツです。",
+                "最後に用意したコンテンツですよ。"];
+            var data2 = data.
+                replace(/@title@/g, title[number]).
+                replace(/@content@/g, content[number]);
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
+            res.write(data2);
             res.end();
         });
-}
- 
+
